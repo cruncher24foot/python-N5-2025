02a3b743f5bbb118a7e6e8fafdfa34d63b507b5e
Michael Onuegbu <M.Onuegbu@albynschool.org>
2025-03-05 15:05:37 +0000

    hi

---
+++ Python/puzzle2.py
@@ -0,0 +1,5 @@
+noChairLegs = ((heightOfTower / heightOfChair) * 2) * 4
+heightOfChair = int(input("Enter the height of one chair: "))
+heightOfTower = int(input("Enter the height of the stacked pair tower: "))
+print("The total number of chair legs in the tower is: "+str(noChairLegs))
+
---
+++ Python/riskystacking.py
@@ -0,0 +1,8 @@
+print("Risky Skating")
+print("=================")
+input("Enter the height of one chair in cm:")
+input("Enter the overall height of the stacked pair tower in cm:")
+print()
+input("Enter the non-negative whole number:")
+input("Enter the positive whole number)
+
